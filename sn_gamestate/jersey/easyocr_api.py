e15ad0dee532a3f6b7dc30d1786ec73d696e2d16
Victor Joos <victor.joos@uclouvain.be>
2024-02-06 11:33:39 +0100

    Fix ocr libraries

--- sn_gamestate/jersey/easyocr_api.py
+++ sn_gamestate/jersey/easyocr_api.py
@@ -6,16 +6,14 @@ import logging
 from tracklab.utils.collate import default_collate, Unbatchable
 from tracklab.pipeline.detectionlevel_module import DetectionLevelModule
 
-
 log = logging.getLogger(__name__)
 
 
 class EasyOCR(DetectionLevelModule):
-    
-    input_columns = ["bbox_ltwh"]
-    output_columns = ["jersey_number", "jn_confidence"]
+    input_columns = []
+    output_columns = ["jersey_number_detection", "jersey_number_confidence"]
     collate_fn = default_collate
-    
+
     def __init__(self, cfg, device, batch_size, tracking_dataset=None):
         super().__init__(batch_size=batch_size)
         self.reader = easyocr.Reader(['en'], gpu=True)
@@ -39,8 +37,8 @@ class EasyOCR(DetectionLevelModule):
 
     @torch.no_grad()
     def process(self, batch, detections: pd.DataFrame, metadatas: pd.DataFrame):
-        jersey_number = []
-        jn_confidence = []
+        jersey_number_detection = []
+        jersey_number_confidence = []
         images_np = [img.cpu().numpy() for img in batch['img']]
         self.reader = easyocr.Reader(['en'], gpu=True)
         if self.batch_size == 1:
@@ -50,7 +48,8 @@ class EasyOCR(DetectionLevelModule):
                 if result == []:
                     jn = self.no_jersey_number()
                 else:
-                    result = result[0]  # only take the first result (highest confidence)
+                    result = result[
+                        0]  # only take the first result (highest confidence)
                     try:
                         # see if the result is a number
                         int(result[1])
@@ -59,15 +58,17 @@ class EasyOCR(DetectionLevelModule):
                     else:
                         jn = [result[0], result[1], result[2]]
 
-                jersey_number.append(jn[1])
-                jn_confidence.append(jn[2])
+                jersey_number_detection.append(jn[1])
+                jersey_number_confidence.append(jn[2])
         else:
-            results = self.reader.readtext_batched(images_np, n_width=64, n_height=128, workers=8, **self.cfg)
+            results = self.reader.readtext_batched(images_np, n_width=64, n_height=128,
+                                                   workers=8, **self.cfg)
             for result in results:
                 if result == []:
                     jn = self.no_jersey_number()
                 else:
-                    result = result[0] # only take the first result (highest confidence)
+                    result = result[
+                        0]  # only take the first result (highest confidence)
                     try:
                         # see if the result is a number
                         int(result[1])
@@ -76,10 +77,10 @@ class EasyOCR(DetectionLevelModule):
                     else:
                         jn = [result[0], result[1], result[2]]
 
-                jersey_number.append(jn[1])
-                jn_confidence.append(jn[2])
+                jersey_number_detection.append(jn[1])
+                jersey_number_confidence.append(jn[2])
 
-        detections['jersey_number'] = jersey_number
-        detections['jn_confidence'] = jn_confidence
-        
+        detections['jersey_number_detection'] = jersey_number_detection
+        detections['jersey_number_confidence'] = jersey_number_confidence
+
         return detections
--- sn_gamestate/jersey/mmocr_api.py
+++ sn_gamestate/jersey/mmocr_api.py
@@ -16,17 +16,17 @@ log = logging.getLogger(__name__)
 
 
 class MMOCR(DetectionLevelModule):
-    
     input_columns = ["bbox_ltwh"]
-    output_columns = ["jersey_number", "jn_confidence"]
+    output_columns = ["jersey_number_detection", "jersey_number_confidence"]
     collate_fn = default_collate
-    
+
     def __init__(self, batch_size, device, tracking_dataset=None):
         super().__init__(batch_size=batch_size)
         self.ocr = MMOCRInferencer(det='dbnet_resnet18_fpnc_1200e_icdar2015', rec='SAR')
         self.batch_size = batch_size
-        
-        self.textdetinferencer = TextDetInferencer('dbnet_resnet18_fpnc_1200e_icdar2015', device=device)
+
+        self.textdetinferencer = TextDetInferencer(
+            'dbnet_resnet18_fpnc_1200e_icdar2015', device=device)
         self.textrecinferencer = TextRecInferencer('SAR', device=device)
 
     def no_jersey_number(self):
@@ -48,23 +48,23 @@ class MMOCR(DetectionLevelModule):
         }
 
         return batch
-    
+
     def extract_numbers(self, text):
         number = ''
         for char in text:
             if char.isdigit():
                 number += char
         return number if number != '' else None
-    
+
     def choose_best_jersey_number(self, jersey_numbers, jn_confidences):
         if len(jersey_numbers) == 0:
             return self.no_jersey_number()
         else:
             jn_confidences = np.array(jn_confidences)
             idx_sort = np.argsort(jn_confidences)
-            return jersey_numbers[idx_sort[-1]], jn_confidences[idx_sort[-1]]   # return the highest confidence jersey number
-            
-    
+            return jersey_numbers[idx_sort[-1]], jn_confidences[
+                idx_sort[-1]]  # return the highest confidence jersey number
+
     def extract_jersey_numbers_from_ocr(self, prediction):
         jersey_numbers = []
         jn_confidences = []
@@ -73,14 +73,14 @@ class MMOCR(DetectionLevelModule):
             if jn is not None:
                 jersey_numbers.append(jn)
                 jn_confidences.append(conf)
-        jersey_number, jn_confidence = self.choose_best_jersey_number(jersey_numbers, jn_confidences)
+        jersey_number, jn_confidence = self.choose_best_jersey_number(jersey_numbers,
+                                                                      jn_confidences)
         return (jersey_number, jn_confidence)
-        
 
     @torch.no_grad()
     def process(self, batch, detections: pd.DataFrame, metadatas: pd.DataFrame):
-        jersey_number = []
-        jn_confidence = []
+        jersey_number_detection = []
+        jersey_number_confidence = []
         images_np = [img.cpu().numpy() for img in batch['img']]
         del batch['img']
         # for img in batch['img']:
@@ -91,28 +91,27 @@ class MMOCR(DetectionLevelModule):
         # with open('images_np.pkl', 'wb') as f:
         #     pickle.dump(images_np, f)
         # predictions = self.ocr(images_np, **self.cfg)['predictions']
-        
+
         predictions = self.run_mmocr_inference(images_np)
         for prediction in predictions:
             jn, conf = self.extract_jersey_numbers_from_ocr(prediction)
-            jersey_number.append(jn)
-            jn_confidence.append(conf)
+            jersey_number_detection.append(jn)
+            jersey_number_confidence.append(conf)
 
-        detections['jersey_number'] = jersey_number
-        detections['jn_confidence'] = jn_confidence
-        
+        detections['jersey_number_detection'] = jersey_number_detection
+        detections['jersey_number_confidence'] = jersey_number_confidence
+
         return detections
-    
 
     def run_mmocr_inference(self, images_np):
         # print('run detection inference')
         result = {}
         result['det'] = self.textdetinferencer(
-                        images_np,
-                        return_datasamples=True,
-                        batch_size=self.batch_size,
-                        progress_bar=False,
-                        )['predictions']
+            images_np,
+            return_datasamples=True,
+            batch_size=self.batch_size,
+            progress_bar=False,
+        )['predictions']
 
         # print('run recognition inference')
         result['rec'] = []
@@ -135,7 +134,7 @@ class MMOCR(DetectionLevelModule):
                     return_datasamples=True,
                     batch_size=self.batch_size,
                     progress_bar=False)['predictions'])
-        
+
         pred_results = [{} for _ in range(len(result['rec']))]
         for i, rec_pred in enumerate(result['rec']):
             result_out = dict(rec_texts=[], rec_scores=[])
@@ -145,6 +144,5 @@ class MMOCR(DetectionLevelModule):
                 result_out['rec_texts'].append(rec_dict_res['text'])
                 result_out['rec_scores'].append(rec_dict_res['scores'])
             pred_results[i].update(result_out)
-            
+
         return pred_results
-    
