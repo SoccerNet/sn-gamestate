74bb348b6a935b0e8bb65b2f6a4c0c85153a2115
Victor Joos <victor.joos@uclouvain.be>
2024-02-09 10:05:53 +0100

    defensive programming

--- sn_gamestate/__init__.py
+++ sn_gamestate/__init__.py
@@ -1,3 +1,5 @@
 from importlib import metadata
-
-__version__ = metadata.version('sn-gamestate')
+try:
+    __version__ = metadata.version('sn-gamestate')
+except metadata.PackageNotFoundError:
+    __version__ = None
