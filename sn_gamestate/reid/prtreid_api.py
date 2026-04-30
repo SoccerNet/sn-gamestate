384a333c69cc599e8752f0775762d331434226c4
Vladimir <somers.vladimir@gmail.com>
2026-04-30 16:25:31 +0200

    Update train method to accept variable arguments

--- sn_gamestate/reid/prtreid_api.py
+++ sn_gamestate/reid/prtreid_api.py
@@ -176,6 +176,6 @@ class PRTReId(DetectionLevelModule):
         )
         return reid_df
 
-    def train(self):
+    def train(self, *args, **kwargs):
         self.engine, self.model = build_torchreid_model_engine(self.cfg)
         self.engine.run(**engine_run_kwargs(self.cfg))
